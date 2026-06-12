7fc909caad6d3ed2207d67a17a3cc7434a7597ee
Dikshant Dwivedi <dikshant.dwivedi@lic.co.nz>
2026-06-12 10:25:57 +0530

    feat(core): configure swagger docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 
@@ -17,6 +18,23 @@ async function bootstrap() {
     })
   );
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle("Conduit API")
+    .setDescription("Django to NestJS migration target API")
+    .setVersion("1.0")
+    .addBearerAuth(
+      {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT",
+        description: "Provide JWT access token"
+      },
+      "bearer"
+    )
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("swagger", app, swaggerDocument);
 
   await app.listen(port);
 }
